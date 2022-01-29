d3dd8fc627192527cba0e5585ee75e68eac4e95d
Tiago <ottiagos@gmail.com>
2022-01-28 23:39:12 -0300

    Testing if has buttons

---
+++ src/components/ButtonsGenerator/ButtonsGenerator.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import ButtonsGenerator from './index';
+
+describe('ButtonsGenerator', () => {
+    it('should render ButtonsGenerator', () => {
+        const ButtonsGeneratorMain = render(<ButtonsGenerator />);
+        expect(ButtonsGeneratorMain.baseElement).toBeInTheDocument();
+    });
+
+    it('should render button text input', () => {
+        const ButtonsGeneratorMain = render(<ButtonsGenerator />);
+        const buttonTextInput = ButtonsGeneratorMain.baseElement.querySelector('[name="button-text"]')
+        expect(buttonTextInput).not.toBeNull();
+        expect(buttonTextInput.tagName).toEqual('INPUT');
+        expect(buttonTextInput.a).toEqual('INPUT');
+    });
+
+    it('should render button color input', () => {
+        const ButtonsGeneratorMain = render(<ButtonsGenerator />);
+        const buttonColoeInput = ButtonsGeneratorMain.baseElement.querySelector('[name="button-color"]')
+        expect(buttonColoeInput).not.toBeNull();
+        expect(buttonColoeInput.tagName).toEqual('INPUT');
+    });
+});
--- src/components/ButtonsGenerator/index.jsx
+++ src/components/ButtonsGenerator/index.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const ButtonsGenerator = () => {
 
     return (
-        <div>Generate custom buttons css code easy</div>
+        <div>Generate custom buttons css code easy
+            <input type="text" name="button-text" placeholder='Button text' />
+            <input type="text" name="button-color" placeholder='Button Color' />
+        </div>
     );
 }
 
